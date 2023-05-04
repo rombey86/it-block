15c5e314783d3f5bd3e4ab197d7c348effd2e809
rombey86 <62323956+rombey86@users.noreply.github.com>
2023-05-04 17:32:14 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,12 +5,6 @@ module.exports = {
       "This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.",
   },
   plugins: [
-        {
-      resolve: "gatsby-plugin-google-analytics",
-      options: {
-        trackingId: "G-MEFG2ZYX26",
-      },
-    },
     "gatsby-plugin-react-helmet",
     {
       resolve: "gatsby-plugin-sass",
